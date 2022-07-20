f4ac47393bf5bbd55514841576d6e843da2f82ae
GitHub Action <action@github.com>
2022-07-20 14:46:45 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/aliut/kotlincollectionsizes/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.aliut.kotlincollectionsizes.services
+
+import com.github.aliut.kotlincollectionsizes.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/aliut/kotlincollectionsizes/services/MyProjectService.kt
+++ src/main/kotlin/com/github/aliut/kotlincollectionsizes/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.aliut.kotlincollectionsizes.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.aliut.kotlincollectionsizes.MyBundle
 
 class MyProjectService(project: Project) {
 
