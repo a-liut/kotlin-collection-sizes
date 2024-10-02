0edea109aa020e2f3f8eaef89222fd417978da1d
a-liut <andrea.liut@gmail.com>
2024-10-02 15:44:10 +0200

    ktlint'd

--- build.gradle.kts
+++ build.gradle.kts
@@ -77,7 +77,7 @@ intellijPlatform {
                     (getOrNull(pluginVersion) ?: getUnreleased())
                         .withHeader(false)
                         .withEmptySections(false),
-                    Changelog.OutputType.HTML,
+                    Changelog.OutputType.HTML
                 )
             }
         }
@@ -143,7 +143,7 @@ intellijPlatformTesting {
                         "-Drobot-server.port=8082",
                         "-Dide.mac.message.dialogs.as.sheets=false",
                         "-Djb.privacy.policy.text=<!--999.999-->",
-                        "-Djb.consents.confirmation.enabled=false",
+                        "-Djb.consents.confirmation.enabled=false"
                     )
                 }
             }
